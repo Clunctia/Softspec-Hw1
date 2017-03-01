b97a35bf29af3041e8a12ac92996b96aaf588419
Goofy <goofy_clunctia@hotmail.com>
2017-03-02 01:06:20 +0900

    Fix some bug and add some bug

--- src/Board.java
+++ src/Board.java
@@ -2,111 +2,131 @@
 public class Board {
 	static int size;
 	static String[][] board;
-	public Board(){
+
+	public Board() {
 		size = 9;
 		board = new String[size][size];
-		for(int i=0 ; i<size ; i++){
-			for(int j=0 ;j<size ; j++){
+		for (int i = 0; i < size; i++) {
+			for (int j = 0; j < size; j++) {
 				board[i][j] = "_";
 			}
 		}
 	}
-	
-	public int getSize(){
+
+	public int getSize() {
 		return size;
 	}
-	
-	public String[][] getBoard(){
+
+	public String[][] getBoard() {
 		return board;
 	}
-	
-	public void placeSymBol(Symbol s, int row, int col){
-		for(int i=0 ; i<size ; i++){
-			for(int j=0 ; j<size ; j++){
-				if(i == row-1 && j == col-1){
+
+	public void placeSymBol(Symbol s, int row, int col) {
+		for (int i = 0; i < size; i++) {
+			for (int j = 0; j < size; j++) {
+				if (i == row - 1 && j == col - 1) {
 					board[i][j] = s.value;
 				}
 			}
 		}
-		
+
 	}
 	
-	public void printBoard(){
-		for(int i=0 ; i<=size ; i++){
-			System.out.print(i+" ");
+	public boolean checkA(int row, int column){
+		if(board[row][column].equals("_")) return true;
+		return false;
+	}
+
+	public void printBoard() {
+		for (int i = 0; i <= size; i++) {
+			System.out.print(i + " ");
 		}
 		System.out.println();
-		for(int i=0 ; i < size ; i++){
-			System.out.print((i+1)+" ");
-			for(int j=0 ; j < size ; j++){
+		for (int i = 0; i < size; i++) {
+			System.out.print((i + 1) + " ");
+			for (int j = 0; j < size; j++) {
 				System.out.print(board[i][j] + " ");
 			}
-			System.out.println();		
+			System.out.println();
 		}
 		System.out.println();
 	}
-	
-	public boolean checkWin(Player player, Board board){
-		boolean win = false;
-		if(checkRow(board, player)){
-			return win = true;
-		}else if(checkColumn(board, player)){
-			return win = true;
-		}else if(checkCross(board, player)){
-			return win = true;
+
+	public boolean checkWin(Player player, Board board) {
+		System.out.println("Checking ....");
+		if (checkRow(player)) {
+			return true;
+		} else if (checkColumn(player)) {
+			return true;
+		} else if (checkDiagonal(player)) {
+			return true;
 		}
-		return win;
+		return false;
 	}
-	
-	public boolean checkCross(Board board, Player player){
-		boolean checked = false;
-		for(int i=0 ; i<board.getSize() ; i++){
-			for(int j=0 ; j<board.getSize() ; j++){
-				
-			}
-		}
-		
-		
-		return checked;
-	}
-	
-	public boolean checkRow(Board board, Player player){
-		boolean checked = false;
+
+	public boolean checkDiagonal(Player player) {
 		int count = 0;
-		for(int i=0 ; i<board.getSize() ; i++){
-			for(int j=0 ; j<board.getSize() ; j++){
-				if(board.getBoard()[i][j].equals(player.getSymValue())){
+		for (int i = 0; i < 5; i++) {
+			for (int j = 0; i + j < 9; j++) {
+				if (board[i + j][j].equals(player.getSymValue())) {
 					count++;
-					if(count == board.getSize()){
+					if (count == 5) {
 						return true;
 					}
-				}else{
+				} else
+					count = 0;
+			}
+		}
+
+		for (int i = 0; i < 5; i++) {
+			for (int j = 0; i + j < 9; j++) {
+				if (board[j][i + j].equals(player.getSymValue())) {
+					count++;
+					if (count == 5) {
+						return true;
+					}
+				} else
+					count = 0;
+			}
+		}
+
+		return false;
+	}
+
+	public boolean checkRow(Player player) {
+		int count = 0;
+		for (int i = 0; i < 9 ; i++) {
+			for (int j = 0; j < 9; j++) {
+				if (board[i][j].equals(player.getSymValue())) {
+					count++;
+					if (count == 5) {
+						return true;
+					}
+				} else {
 					count = 0;
 				}
 			}
 		}
-		
-		return checked;
+
+		return false;
 	}
-	
-	public boolean checkColumn(Board board, Player player){
-		boolean checked = false;
+
+	public boolean checkColumn(Player player) {
 		int count = 0;
-		for(int i=0 ; i<board.getSize() ; i++){
-			for(int j=0 ; j<board.getSize() ; j++){
-				if(board.getBoard()[j][i].equals(player.getSymValue())){
+		for (int i = 0; i < 9; i++) {
+			for (int j = 0; j < 9; j++) {
+				if (board[j][i].equals(player.getSymValue())) {
 					count++;
-					if(count == board.getSize()){
+					if (count == 5) {
 						return true;
 					}
-				}else{
+				} else {
 					count = 0;
 				}
 			}
 		}
-		
-		return checked;
+
+		return false;
 	}
-	
-	
+
 }
--- src/Game.java
+++ src/Game.java
@@ -12,48 +12,44 @@ public class Game {
 
 	public void start() {
 		int row = 0, col = 0;
-		boolean winner = false;
+		boolean win = false;
+
 		scan = new Scanner(System.in);
 		Board board = new Board();
-		System.out.print("Please enter player1 name: ");
-		String name = scan.nextLine();
-		player1 = new Player(name, "X");
-		System.out.print("Please enter player2 name: ");
-		name = scan.nextLine();
-		player2 = new Player(name, "O");
+		player1 = new Player("Player 1", "X");
+		player2 = new Player("Player 2", "O");
 
+		while (!win) {
 
-		
-		System.out.println(player1.toString() + "'s turn.");
-		System.out.print("Please select row: ");
-		row = Integer.parseInt(scan.nextLine());
-		System.out.print("Please select col: ");
-		col = Integer.parseInt(scan.nextLine());
-		player1.placeSymbol(board, row, col);
-		board.printBoard();
-		
-		if(board.checkWin(player1, board)){
-			System.out.println(player1 + " win!!");
-			winner = true;
+			System.out.println(player1.toString() + "'s turn.");
+			System.out.print("Please select row: ");
+			row = Integer.parseInt(scan.nextLine());
+			System.out.print("Please select col: ");
+			col = Integer.parseInt(scan.nextLine());
+			player1.placeSymbol(board, row, col);
+			board.printBoard();
+			System.out.println();
+			if (board.checkWin(player1, board)) {
+				System.out.println(player1 + " win!!");
+				win = true;
+				break;
+			}
+
+			System.out.println(player2.toString() + "'s turn.");
+			System.out.print("Please select row: ");
+			row = Integer.parseInt(scan.nextLine());
+			System.out.print("Please select col: ");
+			col = Integer.parseInt(scan.nextLine());
+			player2.placeSymbol(board, row, col);
+			board.printBoard();
+			System.out.println();
+			if (board.checkWin(player2, board)) {
+				System.out.println(player2 + " win!!");
+				win = true;
+				break;
+			}
 		}
-		
-
-
-		System.out.println(player2.toString() + "'s turn.");
-		System.out.print("Please select row: ");
-		row = Integer.parseInt(scan.nextLine());
-		System.out.print("Please select col: ");
-		col = Integer.parseInt(scan.nextLine());
-		player2.placeSymbol(board, row, col);
-		board.printBoard();
-		
-		if(board.checkWin(player2, board)){
-			System.out.println(player2 + " win!!");
-			winner = true;
-		}
-		
 
 	}
-	
-	
+
 }
